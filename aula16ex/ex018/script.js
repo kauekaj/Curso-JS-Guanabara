37461b0be812f28004e7ea26819d4e9e00c23f59
Kauê Jacyntho <kauekaj@gmail.com>
2022-01-04 11:47:42 -0300

    update ex018 - function finalizar

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -14,11 +14,41 @@ function adicionar() {
             window.alert('Valor inválido ou já cadastrado na lista')
         } else {
             let item = document.createElement('option')
-            item.text = `Valor ${n} adicionado`
+            item.text = `Valor ${n} adicionado.`
             valores.push(n)
-            lista.appendChild(item)          
+            lista.appendChild(item)
+            res.innerHTML = ''         
         }
     num.value = ''
     num.focus()
 }
 
+function finalizar() {
+    if (lista.length == 0) {
+        alert('A lista está vazia')
+    } else {
+    let total = valores.length
+    let maior = valores[0]
+    let menor = valores[0]
+    let soma = 0
+    let media = 0
+
+    for (let pos in valores) {
+        soma += valores[pos]
+        
+        if (valores[pos] > maior)
+            maior = valores[pos]
+        if (valores[pos] < menor)
+            menor = valores[pos]
+        
+    }
+    media = soma / total
+
+    res.innerHTML = ''
+    res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+    res.innerHTML += `<p>O maior número é o ${maior}.</p>`
+    res.innerHTML += `<p>O menor número é o ${menor}.</p>`
+    res.innerHTML += `<p>A soma dos números é ${soma}.</p>`
+    res.innerHTML += `<p>A média dos números é ${media}.</p>`
+    }
+}
